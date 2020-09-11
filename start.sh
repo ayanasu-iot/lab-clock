e13213827f5afe70af7bde4b1c26535d5e67ad45
Kanta Demizu <demizu.1203@gmail.com>
2020-09-11 12:22:04 +0900

    Update file name

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while true
 do
-    /usr/bin/python3 /home/pi/led-demo/clock.py --led-chain=2 --led-slowdown-gpio=2
+    /usr/bin/python3 /home/pi/lab-clock/clock.py --led-chain=2 --led-slowdown-gpio=2
 done
